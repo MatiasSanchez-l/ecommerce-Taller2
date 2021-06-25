1d87a7cc23b4749fd913f26113d469d9cf1138f6
Sanchez Matias <lomatiasleandro@gmail.com>
2021-06-25 10:20:41 -0300

    basic backend structure created and is working

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+const express = require("express");
+const app = express();
+
+//middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//rutas
+app.use("/usuarios", require("./routes/usuarios"));
+app.use("/productos", require("./routes/productos"));
+app.use((err, req, res) => {
+  console.log(err.message)
+});
+
+puerto = process.env.PORT || 5005;
+app.listen(puerto, () => {
+  console.log(`Escuchando el puerto ${puerto}`);
+});
