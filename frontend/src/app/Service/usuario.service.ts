12aedb645fe9260e7f1137403aad56789b4e5ac9
Sanchez Matias <lomatiasleandro@gmail.com>
2021-07-03 13:04:11 -0300

    old code deleted

--- frontend/src/app/Service/usuario.service.ts
+++ frontend/src/app/Service/usuario.service.ts
@@ -1,22 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { Usuario } from '../Model/usuario.model.ts';
 
 @Injectable()
 export class UsuarioService {
-   
-
-  constructor(private httpClient: HttpClient) {}
-  
-  loguearUsuario(usuario:Usuario){
-    return this.httpClient.post<Usuario>(
-        environment.productionUrl + '/usuario/loguear', usuario
-      );
-  }
-
-  desloguearUsuario(){
-
-  }
+ 
 }
