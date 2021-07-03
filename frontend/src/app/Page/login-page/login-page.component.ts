41aec900c65fefffcca336f23f58d4fcc705a1fb
Sanchez Matias <lomatiasleandro@gmail.com>
2021-07-03 13:02:45 -0300

    logic to get auth info

--- frontend/src/app/Page/login-page/login-page.component.ts
+++ frontend/src/app/Page/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Usuario } from 'src/app/Model/usuario.model.ts';
+import { LoginService } from 'src/app/Service/login.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
-  constructor() {}
+  usuario: Usuario;
+  constructor(private loginService: LoginService) {}
 
   ngOnInit(): void {}
+
+  loguear(form: NgForm){
+    const email = form.value.email;
+    const contrasenia = form.value.password;
+    var logueado = this.loginService.loguearUsuario(email, contrasenia);
+    
+  }
 }
