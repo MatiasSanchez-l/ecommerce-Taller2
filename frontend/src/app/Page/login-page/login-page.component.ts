2061dac0f236c8a33a5669a1e608239ccaaaaad2
Sanchez Matias <lomatiasleandro@gmail.com>
2021-07-16 20:54:37 -0300

    frontend login modal now works with backends validations

--- frontend/src/app/Page/login-page/login-page.component.ts
+++ frontend/src/app/Page/login-page/login-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Usuario } from 'src/app/Model/usuario.model.ts';
 import { LoginService } from 'src/app/Service/login.service';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-page',
@@ -20,22 +19,5 @@ export class LoginPageComponent implements OnInit {
     const email = form.value.email;
     const contrasenia = form.value.password;
     this.loginService.loguearUsuario(email, contrasenia);
-    this.logueado = this.loginService.isLogueado();
-
-    if (this.logueado) {
-      Swal.fire({
-        title: 'Bienvenido!',
-        text: '',
-        icon: 'success',
-        confirmButtonText: 'Cool',
-      });
-    } else {
-      Swal.fire({
-        title: 'Error!',
-        text: 'Usuario o contraseña incorrecta',
-        icon: 'error',
-        confirmButtonText: 'Cool',
-      });
-    }
   }
 }
